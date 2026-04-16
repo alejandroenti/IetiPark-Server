e346febb3f987b57e926803f0aec36f19e9c04a5
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2026-04-16 17:59:22 +0200

    Basic server

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Hello IetiPark");
+const { WebSocketServer } = require('ws');
+
+const wss = new WebSocketServer({ port: 3000 });
+console.log('WebSocket server is running on ws://localhost:3000');
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+
+    ws.on('message', (data) => {
+        const message = JSON.parse(data);
+        console.log(`Received message: ${data.toString()}`);
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
